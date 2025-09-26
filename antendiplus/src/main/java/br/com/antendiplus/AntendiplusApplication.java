843ad15a98794541d130e5e74596c798a7a10cef
Nicolas H. Tancon <nicolastancon@gmail.com>
2025-09-26 18:31:09 -0300

    feat (initial commit)

---
+++ antendiplus/src/main/java/br/com/antendiplus/AntendiplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.antendiplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntendiplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntendiplusApplication.class, args);
+    }
+
+}
---
+++ antendiplus/src/test/java/br/com/antendiplus/AntendiplusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antendiplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntendiplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
